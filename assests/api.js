8f82ce5a4b1c81b43281f70224f6e8d172bf17ba
Abdul Moiz <abdulmoiz.az001@gmail.com>
2026-05-14 15:32:31 +0500

    Update api.js

--- assests/api.js
+++ assests/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://192.168.100.74:5000';
+export const BASE_URL = 'http://192.168.141.161:5000';
 
 const handleResponse = async (response) => {
     if (!response.ok) {
